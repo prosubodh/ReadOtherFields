743e3b2c5b70b0fb6be727761ae78ed1101b7a37
Subodh Khanal <prosubodh@gmail.com>
2021-03-09 09:03:29 +0545

    Created a new Component (Initial Code)

---
+++ ReadOtherFields/index.ts
@@ -0,0 +1,80 @@
+import { IInputs, IOutputs } from "./generated/ManifestTypes";
+
+export class ReadOtherFields implements ComponentFramework.StandardControl<IInputs, IOutputs> {
+
+	private mainContainer: HTMLDivElement;
+	private input: HTMLInputElement;
+	private value: string;
+	private _notifyOutputChanged: () => void
+	private tableContainer: HTMLDivElement;
+
+	constructor() {
+	}
+
+	public init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: () => void, state: ComponentFramework.Dictionary, container: HTMLDivElement) {
+		var value: string = 'test';
+		this._notifyOutputChanged = notifyOutputChanged;
+
+		this.mainContainer = document.createElement('div');
+
+		// var label: HTMLLabelElement = document.createElement('label');
+		// label.setAttribute('for', 'value');
+		// label.textContent = "Value: ";
+
+		// this.input = document.createElement('input')
+		// this.input.id = "value"
+		// this.input.type = "text"
+
+		this.tableContainer = document.createElement('div');
+
+		// this.mainContainer.appendChild(label);
+		// this.mainContainer.appendChild(this.input);
+		this.mainContainer.appendChild(this.tableContainer);
+
+		container.appendChild(this.mainContainer);
+	}
+
+	public updateView(context: ComponentFramework.Context<IInputs>): void {
+		// this.value = context.parameters.test.raw!;
+		// this.input.setAttribute("value", context.parameters.test.formatted ? context.parameters.test.formatted : "");
+
+		if (context.parameters.test.raw!) {
+			let xml: string = `<fetch mapping='logical'>  
+			<entity name='crf6e_appointment'>   
+				<attribute name='crf6e_name'/>   
+				<attribute name='crf6e_appointment_date'/> 
+				<filter type='and'>   
+					<condition attribute='crf6e_clinic' operator='eq' value=' ${context.parameters.test.raw!}' />   
+				</filter>
+			</entity>   
+			</fetch>`;
+
+			let that = this;
+			context.webAPI.retrieveMultipleRecords('crf6e_appointment', "?fetchXml=" + xml).then(function (response: ComponentFramework.WebApi.RetrieveMultipleResponse) {
+				var $html = `<table class="appointments">
+					<caption>Clinic Appointments: </caption>
+					<tr>
+						<th>Appointment Name</th>
+						<th>Appointment Date</th>
+					</tr>
+					${response.entities.map(row => `<tr>
+						<td>${row.crf6e_name}</td>
+						<td>${row.crf6e_appointment_date}</td>
+					</tr>`).join('')}
+				</table>`
+				that.tableContainer.innerHTML = $html;
+			}, function (error) {
+				console.error("Error: ", error)
+			})
+		}
+	}
+
+	public getOutputs(): IOutputs {
+		return {
+			// test: this.value
+		};
+	}
+
+	public destroy(): void {
+	}
+}
